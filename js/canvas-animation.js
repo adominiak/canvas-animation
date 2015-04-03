7cba602a897a4585e6adc17d9259fde1be7ea7d2
Anna A Dominiak <a.dominiak@primemodule.com>
2015-04-03 14:08:06 +0200

    changed code structure

--- js/canvas-animation.js
+++ js/canvas-animation.js
@@ -1,25 +1,5 @@
 (function(){
-  var canvas = document.getElementById('CanvasForPhotos');
-  if(canvas.getContext){
-
-    var context = canvas.getContext('2d');
-    var imgOne = {
-      image: new Image(),
-      x: 50,
-      y:50
-    }
-    var imgTwo = {
-      image: new Image(),
-      x: 50,
-      y:300
-    }
-    var renderImages = function(images) {
-      context.clearRect(0, 0, canvas.width, canvas.height);
-      images.forEach( function( image ) {
-        context.drawImage(image.image, image.x, image.y);
-      } );
-    }
-    var requestAnimationFrame =
+  var requestAnimationFrame =
       window.requestAnimationFrame ||
       window.webkitRequestAnimationFrame ||
       window.mozRequestAnimationFrame ||
@@ -28,53 +8,84 @@
       function(callback) {
         return setTimeout(callback, 1);
       };
-
-    imgOne.image.src = 'http://lorempixel.com/400/200/';
-    imgTwo.image.src = 'http://lorempixel.com/300/200/';
-    var imagesLoaded = false;
-    var imageLoadedCallback = function(image) {
-      return function() {
-        if(imagesLoaded) {
-          renderImages(images);
-          bothImagesLoaded();
-        }
-        else {
-          imagesLoaded = true;
-        }
-      }
-    }
-    var images = [
-      imgOne,
-      imgTwo
-    ];
-    imgOne.image.onload = imageLoadedCallback(imgOne);
-    imgTwo.image.onload = imageLoadedCallback(imgTwo);
-
-    var bothImagesLoaded = function() {
-      var button = document.getElementById("BtnMovePhotos");
-      var callbackMoveImage = function(images) {
-        return function() {
-          moveImages(images);
-        }
-      }
-      var moveImages = function(images) {
-        var callAgain = false;
-        renderImages(images);
-        for (var i = images.length - 1; i >= 0; i--) {
-          if(images[i].x+images[i].image.width < canvas.width) {
-            images[i].x++;
-            callAgain = true;
-          }
-        };
-        if(callAgain) {
-          requestAnimationFrame(callbackMoveImage(images));
-        }
-      }
-      var activateMove = function() {
-        moveImages(images);
-      }
-      button.hidden="";
-      button.addEventListener("click", activateMove);
-    };
+  function PhotoCanvas() {
+    this.canvas = document.getElementById('CanvasForPhotos');
+    this.setup();
   }
+
+  PhotoCanvas.prototype.setup = function(image) {
+    if(this.canvas.getContext){
+      this.context = this.canvas.getContext('2d');
+      this.imagesLoaded = false;
+      var imgOne = {
+        image: new Image(),
+        x: 50,
+        y:50
+      }
+      var imgTwo = {
+        image: new Image(),
+        x: 50,
+        y: 300
+      }
+      imgOne.image.src = 'http://lorempixel.com/400/200/';
+      imgTwo.image.src = 'http://lorempixel.com/300/200/';
+      this.images = [
+        imgOne,
+        imgTwo
+      ];
+      imgOne.image.onload = this.imageLoadedCallback(imgOne);
+      imgTwo.image.onload = this.imageLoadedCallback(imgTwo);
+    }
+  }
+
+  PhotoCanvas.prototype.renderImages = function(images) {
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    images.forEach( (function( image ) {
+      this.context.drawImage(image.image, image.x, image.y);
+    }).bind(this) );
+  }
+
+
+  PhotoCanvas.prototype.imageLoadedCallback = function(image) {
+    var self = this;
+        return function() {
+          if(self.imagesLoaded) {
+            self.renderImages(self.images);
+            self.bothImagesLoaded();
+          }
+          else {
+            self.imagesLoaded = true;
+          }
+        }
+      }
+  PhotoCanvas.prototype.moveImages = function(images) {
+    var callAgain = false;
+    this.renderImages(images);
+    for (var i = images.length - 1; i >= 0; i--) {
+      if(images[i].x+images[i].image.width < this.canvas.width) {
+        images[i].x++;
+        callAgain = true;
+      }
+    };
+    if(callAgain) {
+      requestAnimationFrame(this.callbackMoveImage(images).bind(this));
+    }
+  }
+  PhotoCanvas.prototype.callbackMoveImage = function(images) {
+    return function() {
+      this.moveImages(images);
+    }
+  }
+  PhotoCanvas.prototype.bothImagesLoaded = function() {
+    var button = document.getElementById("BtnMovePhotos");
+    var activateMove = function() {
+      this.moveImages(this.images);
+    }
+    button.hidden = "";
+    button.addEventListener("click", activateMove.bind(this));
+  };
+
+  new PhotoCanvas();
+  window.PhotoCanvas = window.PhotoCanvas || {};
 }());
+
